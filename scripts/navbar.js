9519ffc2294ebbe070207fb7e736cabf80ffec06
Mr_Expence <zedstarnight@gmail.com>
2026-08-13 17:19:53 +1000

    Refactor code structure for improved readability and maintainability

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -19,16 +19,16 @@ const html = `<header class="nav">
       </label>
       <div class="nav__actions">
         <button class="icon-btn theme-toggle" data-theme-toggle aria-label="Toggle dark mode">
-          <svg class="icon-sun" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg class="icon-sun" viewBox="0 -960 960 960" width="24px" height="24px" fill="#b7bac3" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="4.5" stroke="currentColor" stroke-width="1.6" />
             <path
-              d="M12 2.5v2M12 19.5v2M21.5 12h-2M4.5 12h-2M18.4 5.6l-1.4 1.4M7 17l-1.4 1.4M18.4 18.4L17 17M7 7 5.6 5.6"
+              d="M565-395q35-35 35-85t-35-85q-35-35-85-35t-85 35q-35 35-35 85t35 85q35 35 85 35t85-35Zm-226.5 56.5Q280-397 280-480t58.5-141.5Q397-680 480-680t141.5 58.5Q680-563 680-480t-58.5 141.5Q563-280 480-280t-141.5-58.5ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"
               stroke="currentColor" stroke-width="1.6" stroke-linecap="round" />
           </svg>
-          <svg class="icon-moon" viewBox="0 0 24 24" fill="none" style="color: black" xmlns="http://www.w3.org/2000/svg"
+          <svg class="icon-moon" viewBox="0 -960 960 960" width="24px" height="24px" fill="#b7bac3" xmlns="http://www.w3.org/2000/svg"
             style="display:none">
-            <path d="M20 14.5A8 8 0 1 1 9.5 4a6.4 6.4 0 0 0 10.5 10.5Z" stroke="currentColor" stroke-width="1.6"
-              stroke-linejoin="round" />
+            <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" stroke="currentColor" stroke-width="1.6"
+              stroke="currentColor" stroke-width="1.6" stroke-linecap="round" />
           </svg>
         </button>
         <button class="icon-btn nav__menu-btn" data-menu-toggle aria-label="Toggle menu" aria-expanded="false">
